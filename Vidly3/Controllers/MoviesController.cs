ae98f8e90563ee6bf77689bbf06b87f5fe5c7340
MANRAJSINGH007 <manrajs2014@gmail.com>
2020-01-20 18:13:37 +0530

    add another way to pass the data to the views

--- Vidly3/Controllers/MoviesController.cs
+++ Vidly3/Controllers/MoviesController.cs
@@ -13,7 +13,9 @@ namespace Vidly3.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            var viewResult = new ViewResult();
+            viewResult.ViewData.Model = movie;
+            return viewResult;
         }
 
         public ActionResult Edit(int id)
@@ -28,6 +30,9 @@ namespace Vidly3.Controllers
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
+        //The regular expression here is not a string so we have to repeat the backslash twice
+        //[Route("movies/released/{year}/{month: regex(\\d{4}):regex:range(1, 12)}")]
+        [Route("movies/released/{year}/{month}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
