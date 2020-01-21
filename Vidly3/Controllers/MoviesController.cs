778ef86e7ecdd62a2ab45d7bbba6a850ab147a63
MANRAJSINGH007 <manrajs2014@gmail.com>
2020-01-21 16:50:11 +0530

    add genre table and add fileds to movie and display the details of movies when clicked

--- Vidly3/Controllers/MoviesController.cs
+++ Vidly3/Controllers/MoviesController.cs
@@ -5,16 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly3.Models;
 using Vidly3.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly3.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null) return HttpNotFound();
+            return View(movie);
         }
 
         private IEnumerable<Movie> GetMovies()
