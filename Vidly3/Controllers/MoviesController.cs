db002b30b1575be7d6fa75755bec89fd7be8a3f4
MANRAJSINGH007 <manrajs2014@gmail.com>
2020-01-23 10:25:56 +0530

    add form to add or edit an existing movie

--- Vidly3/Controllers/MoviesController.cs
+++ Vidly3/Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Vidly3.Models;
 using Vidly3.ViewModels;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 
 namespace Vidly3.Controllers
 {
@@ -27,6 +28,57 @@ namespace Vidly3.Controllers
         {
             var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null) return HttpNotFound();
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            // Specify the viewname else the MVC will look for a view with the name Edit
+            return View("Create", viewModel);
+        }
+
+        public ActionResult Create()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDB = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDB.Name = movie.Name;
+                //movieInDB.Genre = movie.Genre;
+                movieInDB.GenreId = movie.GenreId;
+                movieInDB.ReleaseDate = movie.ReleaseDate;
+                movieInDB.NumberInStock = movie.NumberInStock;
+            }
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch(DbEntityValidationException e)
+            {
+                Console.WriteLine(e);
+            }
+            return RedirectToAction("Index", "Movies");
         }
 
         public ActionResult Details(int id)
@@ -62,10 +114,10 @@ namespace Vidly3.Controllers
             return View(viewModel);
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id = " + id);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id = " + id);
+        //}
 
         //public ActionResult Index(int? pageIndex, string sortBy)
         //{
